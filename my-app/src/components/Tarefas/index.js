d6edee9ddfc983bac7b87fc50d7b4c83318d6ea6
kaxkata <gabrielr387@gmail.com>
2020-11-09 19:28:44 -0300

    criacao pagina meus tickets

--- my-app/src/components/Tarefas/index.js
+++ my-app/src/components/Tarefas/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
 import ConteudoHeader from '../ConteudoHeader';
-
+import CardsKamban from '../Cards-Kamban/card-kamban';
+import CardsChamado from '../Cards-Chamado/card-chamado';
 import './style.css';
 
 export default function Tarefas() {
@@ -13,6 +14,16 @@ export default function Tarefas() {
                     titulo='Tarefas' 
                     descricao='Veja as tarefas concluídas, não concluídas e em progresso.'
                 />
+                <CardsKamban cor='card-meuschamados-agente' value='Chamados' name='Meus Chamados' />
+                <CardsKamban cor='card-test' value='Chamados' name='Meus Chamados' />
+                <CardsKamban cor='card-meuschamados-agente' value='Chamados' name='Meus Chamados' />
+                <CardsKamban cor='card-meuschamados-agente' value='Chamados' name='Meus Chamados' />
+                <CardsChamado/>
+                <CardsChamado/>
+                <CardsChamado/>
+                <CardsChamado/>
+        
+
             </main>
 
         </section>
