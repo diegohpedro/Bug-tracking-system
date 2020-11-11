1e90971ca474c99cfd0beee23c9b3981292bf89d
João Victor <victorps.joao@gmail.com>
2020-11-11 11:14:07 -0300

    Merge

--- my-app/src/components/componentsAdmin/Tarefas/index.js
+++ my-app/src/components/componentsAdmin/Tarefas/index.js
@@ -15,19 +15,8 @@ export default function Tarefas() {
                     titulo='Tarefas' 
                     descricao='Veja as tarefas concluídas, não concluídas e em progresso.'
                 />
-<<<<<<< HEAD
 
                 
-=======
-                <CardsKamban cor='card-ticketcriado' value='Criados' name='Tickets Criados' />
-                <CardsKamban cor='card-ticketsemresposta' value='Sem resposta' name='Ticket sem resposta' />
-                <CardsKamban cor='card-ticketandamento' value='Em andamento' name='Tickets em andamento' />
-                <CardsKamban cor='card-ticketconcluido' value='Concluidos' name='Tickets Concluidos' />
-                <CardsChamado/>
-                <CardsChamado/>
-                <CardsChamado/>
-                <CardsChamado/>
->>>>>>> 441cb2f8dc1947f75412d886ff037bb3c5a0cd62
             </main>
         </section>
     )
